7ce2e3b491dd219b9f07739fb50dd26443cbd39b
Oskars AA <66279792+Krumuvecis@users.noreply.github.com>
2022-05-09 01:44:26 +0300

    0.3
    improvements

--- src/main/java/DelayCalculator/DelayCalculator.java
+++ src/main/java/DelayCalculator/DelayCalculator.java
@@ -1,40 +1,55 @@
 package DelayCalculator;
 
+import java.util.Date;
+
 import com.google.inject.internal.Nullable;
 
-//TODO: finish this
+//TODO: finish this and add javadoc
 public class DelayCalculator {
 
+    private static final long MINIMUM_DELAY = 0;
 
     private DelayOptions options;
+    private long
+            startingTime,
+            elapsedTime;
 
+    @SuppressWarnings("unused")
     public DelayCalculator() {
         options = new DelayOptions();
     }
 
+    @SuppressWarnings("unused")
     public DelayCalculator(long delay) {
         options = new DelayOptions(delay);
     }
 
+    @SuppressWarnings("unused")
     public DelayCalculator(@Nullable DelayOptions delayOptions) {
         options = new DelayOptions(delayOptions);
     }
 
-
-
-    //set max FPS/delay
-    //get at start
-    //get at end
-    //return difference
-
-    //calculate delay from difference and max
-
-    public long getDelay() {
-        return options.getPreferredMS();
+    public void start() {
+        startingTime = getCurrentTime();
     }
 
-    //calculate average delta and corresponding delay
+    public void end() {
+        elapsedTime = getCurrentTime() - startingTime;
+    }
 
+    private static long getCurrentTime() {
+        return (new Date()).getTime();
+    }
 
+    public long getDelay() {
+        return Math.max(
+                MINIMUM_DELAY,
+                options.getPreferredMS() - elapsedTime);
+    }
 
+    public long getElapsedTime() {
+        return elapsedTime;
+    }
+
+    //TODO: calculate average elapsed time
 }
