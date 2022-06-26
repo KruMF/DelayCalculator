c000383171ec793bd9bcb95086c08a5ee28c836b
Oskars AA <66279792+Krumuvecis@users.noreply.github.com>
2022-06-26 18:08:31 +0300

    0.4.1
    version update and some javadoc

--- src/main/java/DelayCalculator/DelayCalculator.java
+++ src/main/java/DelayCalculator/DelayCalculator.java
@@ -2,9 +2,13 @@ package DelayCalculator;
 
 import java.util.Date;
 
-import com.google.inject.internal.Nullable;
+import org.jetbrains.annotations.Nullable;
 
-//TODO: finish this and add javadoc
+/**
+ * TODO:
+ *      * calculate average elapsed time over multiple cycles (see somewhere near the end of this file)
+ *      * finish javadocs
+ */
 public class DelayCalculator {
 
     private static final long MINIMUM_DELAY = 0;
@@ -14,25 +18,47 @@ public class DelayCalculator {
             startingTime,
             elapsedTime;
 
+    /**
+     * Creates a DelayCalculator with default delay in milliseconds.
+     */
     @SuppressWarnings("unused")
     public DelayCalculator() {
         options = new DelayOptions();
     }
 
+    /**
+     * Creates a DelayCalculator with delay specified in milliseconds.
+     *
+     * @param delay Delay in milliseconds.
+     */
     @SuppressWarnings("unused")
     public DelayCalculator(long delay) {
         options = new DelayOptions(delay);
     }
 
+    /**
+     * Creates a DelayCalculator with specified DelayOptions.
+     *      (Null DelayOptions -> default type and value)
+     *
+     * @param delayOptions Delay options object.
+     */
     @SuppressWarnings("unused")
     public DelayCalculator(@Nullable DelayOptions delayOptions) {
         options = new DelayOptions(delayOptions);
     }
 
+    /**
+     * Starts measuring elapsed time.
+     * Call this at the start of a cycle.
+     */
     public void start() {
         startingTime = getCurrentTime();
     }
 
+    /**
+     * Stops measuring elapsed time.
+     * Call this at the end of a cycle.
+     */
     public void end() {
         elapsedTime = getCurrentTime() - startingTime;
     }
@@ -41,15 +67,25 @@ public class DelayCalculator {
         return (new Date()).getTime();
     }
 
+    /**
+     * Gets the appropriate delay.
+     * Call this when specifying delay for Thread.sleep().
+     */
     public long getDelay() {
         return Math.max(
                 MINIMUM_DELAY,
                 options.getPreferredMS() - elapsedTime);
     }
 
+    /**
+     * Gets the elapsed time from start() to end() (one full cycle for continuous cycles).
+     * Call this before start() or after end() for meaningful results.
+     *
+     * @return Elapsed time in milliseconds.
+     */
     public long getElapsedTime() {
         return elapsedTime;
     }
 
-    //TODO: calculate average elapsed time
+    //TODO: calculate average elapsed time over multiple cycles
 }
