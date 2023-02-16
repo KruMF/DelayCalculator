8774c11bfbacc3512f357fe818b0cfd098510be2
Oskars AA <66279792+Krumuvecis@users.noreply.github.com>
2023-02-16 04:54:07 +0200

    work on tests

--- src/test/java/delayOptionsTest/DelayOptionsContainer.java
+++ src/test/java/delayOptionsTest/DelayOptionsContainer.java
@@ -2,31 +2,73 @@ package delayOptionsTest;
 
 import org.jetbrains.annotations.NotNull;
 
+import consoleUtils.bufferedPrinting.BufferSection;
+import consoleUtils.bufferedPrinting.AbstractConsoleBuffer;
+import consoleUtils.bufferedPrinting.SingleSectionConsoleBuffer;
+
 import delayCalculator.DelayOptions;
 
-//TODO: finish this
+//TODO: finish this and add javadocs
 abstract class DelayOptionsContainer {
     private final @NotNull DelayOptions delayOptions;
-    private final @NotNull String delayOptionsName;
+    private final @NotNull AbstractConsoleBuffer consoleBuffer;
 
-    //TODO: add javadoc
-    DelayOptionsContainer(@NotNull String delayOptionsName) {
+    /**
+     * TODO: finish this javadoc
+     */
+    protected DelayOptionsContainer(@NotNull String delayOptionsName) {
         delayOptions = getInitialDelayOptions();
-        this.delayOptionsName = delayOptionsName;
+        consoleBuffer = new SingleSectionConsoleBuffer(
+                new DelayOptionsBufferSection(delayOptionsName, this)
+        );
     }
 
-    //TODO: add javadoc
-    abstract @NotNull DelayOptions getInitialDelayOptions();
+    /**
+     * TODO: finish this javadoc
+     */
+    protected abstract @NotNull DelayOptions getInitialDelayOptions();
 
-    //TODO: add javadoc
-    final @NotNull DelayOptions getDelayOptions() {
+    /**
+     * TODO: finish this javadoc
+     */
+    protected final @NotNull DelayOptions getDelayOptions() {
         return delayOptions;
     }
 
-    //TODO: finish this
-    final void print() {
-        String a = "delay options type: " + delayOptionsName;
-        String b = "MS: " + delayOptions.getPreferredMS();
-        String c = "FPS: " + delayOptions.getPreferredFPS();
+    /**
+     * TODO: finish this javadoc
+     */
+    protected final void print() {
+        consoleBuffer.print();
+    }
+
+    /**
+     * TODO: finish this javadoc
+     */
+    private static final class DelayOptionsBufferSection extends BufferSection {
+        private final @NotNull String delayOptionsName;
+        private final @NotNull DelayOptionsContainer container;
+
+        /**
+         * TODO: finish this javadoc
+         */
+        private DelayOptionsBufferSection(@NotNull String delayOptionsName,
+                                          @NotNull DelayOptionsContainer container) {
+            super();
+            this.delayOptionsName = delayOptionsName;
+            this.container = container;
+        }
+
+        /**
+         * Gets the contents of this buffer section.
+         * Override this to set the actual contents.
+         */
+        @Override
+        public void getContents() {
+            addLine("Delay options type: " + delayOptionsName);
+            @NotNull DelayOptions delayOptions = container.getDelayOptions();
+            addLine("MS: " + delayOptions.getPreferredMS());
+            addLine("FPS: " + delayOptions.getPreferredFPS());
+        }
     }
 }
